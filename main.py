4f5b51d2b800dc783db06f11fcfa8187f8ee6627
amirinbar <amir.inbar@lynx.md>
2022-10-07 13:16:23 +0300

    upload backend

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from todo.database import engine
+from todo import models
+
+models.Base.metadata.create_all(engine)
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
