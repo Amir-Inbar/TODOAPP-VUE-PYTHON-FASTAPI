b078f90caab555d4a132d004a574c37ccdad5480
amirinbar <amir.inbar@lynx.md>
2022-10-07 15:16:21 +0300

    uploading to heroku

--- main.py
+++ main.py
@@ -4,21 +4,12 @@ from todo.database import engine
 from todo import models
 import uvicorn
 
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+
 
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="public")
-app.mount("./public", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
 
 origins = [
--- todo/main.py
+++ todo/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .database import engine
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .routes import todo, user, authentication
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -12,6 +15,14 @@ origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
+
+templates = Jinja2Templates(directory="public")
+app.mount("./public", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 app.add_middleware(
