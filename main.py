26f422461261fab52a7e156770ea1fc43817f952
amirinbar <amir.inbar@lynx.md>
2022-10-07 13:46:52 +0300

    uploading to heroku

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from todo.database import engine
 from todo import models
@@ -6,6 +7,9 @@ from todo import models
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.mount("./public", StaticFiles(directory="public"), name="public")
+
 
 origins = [
     "http://localhost:3000",
